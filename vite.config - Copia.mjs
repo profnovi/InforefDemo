5121755193befea5c0c691dc5cf4c7fda60eb639
profnovi <73852176+profnovi@users.noreply.github.com>
2025-10-28 09:43:53 +0100

    ee

--- vite.config - Copia.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-const isProd = process.env.NODE_ENV === 'production'
-
-export default defineConfig({
-  base: isProd ? '/Inforef_DEMO/' : '/',
-  plugins: [vue()],
-})
--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const isProd = process.env.NODE_ENV === 'production'
+
 export default defineConfig({
-  base: '/InforefDemo/',
-  plugins: [vue()]
+  base: isProd ? '/Inforef_DEMO/' : '/',
+  plugins: [vue()],
 })
