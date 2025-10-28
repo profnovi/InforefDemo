5223d8b3a53bf25cadf8304abcb834ed5891f694
profnovi <73852176+profnovi@users.noreply.github.com>
2025-10-28 09:41:12 +0100

    Secondo commit

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const isProd = process.env.NODE_ENV === 'production'
-
 export default defineConfig({
-  base: isProd ? '/Inforef_DEMO/' : '/',
-  plugins: [vue()],
+  base: '/InforefDemo/',
+  plugins: [vue()]
 })
